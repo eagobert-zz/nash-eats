9509706ceaa92c613b4948cf8c31dc32998f811d
eagobert <eagobert@hotmail.com>
2018-09-07 16:23:06 -0500

    navbar partially complete

--- NashBitesApp/urls.py
+++ NashBitesApp/urls.py
@@ -2,9 +2,16 @@ from django.urls import path
 from . import views
 
 
+app_name = "website"
+
 urlpatterns = [
-  path('', views.Welcome, name='Welcome'),
+  
+  # Django provides a built in url for login:
+  # auth/login/ [name='login'] under include('django.contrib.auth.urls')
+  # in NashBitesProject urls.py file
+
+  path('', views.Welcome, name='welcome'),
   path('register/', views.Register, name='register'),
-  path('vendor/', views.Vendor ,name='VendorView'),
-  path('user/', views.User ,name='UserView'),
+  path('vendor/', views.Vendor ,name='vendor'),
+  path('home/', views.HomeView.as_view() ,name='home'),
 ]
