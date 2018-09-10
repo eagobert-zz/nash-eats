baba5bf9eeb48524da8f2ef9a377238ccba016de
eagobert <eagobert@hotmail.com>
2018-09-10 09:19:59 -0500

    add vendor view

---
+++ NashBitesApp/static/js/main.js
@@ -0,0 +1,5 @@
+//Need Event handler to hide search input on click of login or register affordance
+
+//Need Event handler to manage home view map
+
+//Need Event handler to manage vendor view map
--- NashBitesApp/views/vendor.py
+++ NashBitesApp/views/vendor.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+
 
 @login_required
-def Vendor(request):
-    return HttpResponse("Hello, world. You're at the Nash Bites App vendor admin view.")
+class VendorView(TemplateView):
+    template_name = 'vendor.html'
