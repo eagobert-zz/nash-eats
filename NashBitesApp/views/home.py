e992d9f924d28b482b5e84ac51eb1f1b0eb3c8e7
eagobert <eagobert@hotmail.com>
2018-09-14 13:21:25 -0500

    receiving search input and geocode data

--- NashBitesApp/views/home.py
+++ NashBitesApp/views/home.py
@@ -12,20 +12,23 @@ def HomeView(request):
     Methods:  GET, POST
     """
     if request.method == 'GET':
-        search_form = SearchForm()
         search_input = request.GET.get('search-input', None)
         api_key = getattr(settings, 'GOOGLE_MAPS_API_KEY')
         template_name = 'home/home.html'
-        context = {
-            'api_key': api_key,
-            'search_form': search_form,
-        }
 
         if search_input:
             print(search_input)
-            # gm = googlemaps.Client(key=api_key)
-            # geocode_result = gm.geocode('1600 Amphitheatre Parkway, Mountain View, CA')[0]
+            gm = googlemaps.Client(key=api_key)
+            geocode_result = gm.geocode(search_input)[0]
+            geocode_result_origin = geocode_result['formatted_address']
+            geocode_result_location = geocode_result['geometry']['location']
 
+        context = {
+            'api_key': api_key,
+            'search_input': search_input,
+            'origin': geocode_result_origin,
+            'origin_location': geocode_result_location,
+        }
         return render(request, template_name, context)
 
     if request.method == 'POST':
