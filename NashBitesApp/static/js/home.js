0e1f506b422f46dea6291f0ad897c702e49a934b
eagobert <eagobert@hotmail.com>
2018-09-18 17:07:29 -0500

    receiving origin from search input

--- NashBitesApp/static/js/home.js
+++ NashBitesApp/static/js/home.js
@@ -3,20 +3,37 @@
 
 // Declare global variables
 var map;
+var geocoder
 var marker; 
 var infowindow;
 var messagewindow;
 
-var searchEl = document.getElementById('search-input')
-
 // Function initializes and adds the Nashville map w/o marker
 function initMap() {
 
+  geocoder = new google.maps.Geocoder();
+  
   // The location of Nashville, TN
   const nashville = {lat: 36.174465, lng: -86.767960};
 
   // The map, centered at Nashville, TN
   map = new google.maps.Map(
       document.getElementById('home-map'), {zoom: 12, center: nashville});
-
+  
+    }
+    
+  function codeAddress() {
+      var address = document.getElementById('address').value;
+      geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == 'OK') {
+      console.log(results)
+      map.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
 }
--- NashBitesApp/views/home.py
+++ NashBitesApp/views/home.py
@@ -15,50 +15,13 @@ def HomeView(request):
     Methods:  GET, POST
     """
     if request.method == 'GET':
-        search_input = request.GET.get('search-input', None)
+        # search_input = request.GET.get('search-input', None)
         api_key = getattr(settings, 'GOOGLE_MAPS_API_KEY')
         template_name = 'home/home.html'
-        gm = googlemaps.Client(key=api_key)
-
-        if search_input:
-            # Get origin: Geocode data from search input
-            geocode_result = gm.geocode(search_input)[0]
-            origin = geocode_result['formatted_address']
-            geocode_result_location = geocode_result['geometry']['location']
-
-            # Get destinations: create query_result = list of dictionaries of all vendor's most current timestamp location
-            query_results = Location.objects.values().order_by('-timestamp')
-
-            seen = list()
-            destinations = []
-
-            # Loop over query results
-            for result in query_results:
-
-                # If value of vendor_id is not in empty list
-                if result['vendor_id'] not in seen:
-
-                    # append to destinations list
-                    destinations.append(result['address'])
-
-                    # then append value of vendor_id to seen list that results in having only the most current result for each vendor in destinations list
-                    seen.append(result['vendor_id'])
-
-                
-        # Get distance data from Google based on destination and origin
-        if destinations:
-           dist_results = gm.distance_matrix(origin, destinations)
-           print(dist_results)
-        # make a marker for the 5 closest vendors
-
-            # place marker on map
 
         context = {
             'api_key': api_key,
-            'search_input': search_input,
-            'origin': origin,
-            'origin_location': geocode_result_location,
-            'destinations': destinations,
+            # 'search_input': search_input,
         }
         return render(request, template_name, context)
 
