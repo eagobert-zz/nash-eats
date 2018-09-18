f136ffee7840e3188da2609e37bc7dac695acf14
eagobert <eagobert@hotmail.com>
2018-09-18 10:46:34 -0500

    changed variable names in home view

--- NashBitesApp/views/home.py
+++ NashBitesApp/views/home.py
@@ -26,34 +26,39 @@ def HomeView(request):
             geocode_result_origin = geocode_result['formatted_address']
             geocode_result_location = geocode_result['geometry']['location']
 
-            # Get destinations: create array of all vendor's most current location
-            destinations = Location.objects.values().order_by('-timestamp')
+            # Get destinations: create array of all vendor's most current timestamp location
+            query_results = Location.objects.values().order_by('-timestamp')
 
-            
             seen = list()
-            keep = []
+            destinations = []
 
-            # Loop over the list of dictionaries
-            for destination in destinations:
-                if destination['vendor_id'] not in seen:
-                    keep.append(destination)
-                    seen.append(destination['vendor_id'])
-                    print(keep)
+            # Loop over the list of query_results
+            for result in query_results:
+
+                # If value of vendor_id is not in empty list
+                if result['vendor_id'] not in seen:
+
+                    # append to destinations list
+                    destinations.append(result)
+
+                    # then append value of vendor_id to seen list
+                    # This results in having only the most current result for each vendor in destinations list
+                    seen.append(result['vendor_id'])
+
+                print(destinations)
                 
-
             # Get distance data from Google based on destination and origin
 
             # make a marker for the 5 closest vendors
 
             # place marker on map
 
-
         context = {
             'api_key': api_key,
             'search_input': search_input,
             'origin': geocode_result_origin,
             'origin_location': geocode_result_location,
-            'destinations': keep,
+            'destinations': destinations,
         }
         return render(request, template_name, context)
 
