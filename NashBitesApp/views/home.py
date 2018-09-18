099f2a3e85bbea234da8cb0ca984431904499ebc
eagobert <eagobert@hotmail.com>
2018-09-18 14:55:38 -0500

    getting vendor distance

--- NashBitesApp/views/home.py
+++ NashBitesApp/views/home.py
@@ -18,12 +18,13 @@ def HomeView(request):
         search_input = request.GET.get('search-input', None)
         api_key = getattr(settings, 'GOOGLE_MAPS_API_KEY')
         template_name = 'home/home.html'
+        gm = googlemaps.Client(key=api_key)
 
         if search_input:
             # Get origin: Geocode data from search input
-            gm = googlemaps.Client(key=api_key)
             geocode_result = gm.geocode(search_input)[0]
-            geocode_result_origin = geocode_result['formatted_address']
+            origin = geocode_result['formatted_address']
+            print(origin)
             geocode_result_location = geocode_result['geometry']['location']
 
             # Get destinations: create array of all vendor's most current timestamp location
@@ -39,24 +40,25 @@ def HomeView(request):
                 if result['vendor_id'] not in seen:
 
                     # append to destinations list
-                    destinations.append(result)
+                    destinations.append(result['address'])
 
                     # then append value of vendor_id to seen list
                     # This results in having only the most current result for each vendor in destinations list
                     seen.append(result['vendor_id'])
-
-                print(destinations)
+            # print(destinations)
                 
-            # Get distance data from Google based on destination and origin
-
-            # make a marker for the 5 closest vendors
+        # Get distance data from Google based on destination and origin
+        if destinations:
+           dist_results = gm.distance_matrix(origin, destinations)
+           print(dist_results)
+        # make a marker for the 5 closest vendors
 
             # place marker on map
 
         context = {
             'api_key': api_key,
             'search_input': search_input,
-            'origin': geocode_result_origin,
+            'origin': origin,
             'origin_location': geocode_result_location,
             'destinations': destinations,
         }
