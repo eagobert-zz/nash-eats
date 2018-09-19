4f3585e084f644b762d3c4598405d1380c1f095b
eagobert <eagobert@hotmail.com>
2018-09-19 13:55:54 -0500

    register view almost complete

--- NashBitesApp/forms/registration_form.py
+++ NashBitesApp/forms/registration_form.py
@@ -6,3 +6,9 @@ class RegistrationForm(forms.ModelForm):
   class Meta:
     model = Profile
     fields = ['company_name',]
+    labels = {
+      'company_name': '',
+    }
+    widgets = {
+      'company_name': forms.TextInput(attrs={'placeholder': 'Company Name'})
+    }
--- NashBitesApp/forms/user_form.py
+++ NashBitesApp/forms/user_form.py
@@ -3,6 +3,21 @@ from django import forms
 
 
 class UserForm(forms.ModelForm):
+  password = forms.CharField(
+    label="", 
+    widget=forms.PasswordInput
+  )
+
   class Meta:
     model = User
     fields = ['username', 'password']
+    labels = {
+      'username': '',
+      # 'password': ''
+    }
+    help_texts = {
+      'username': '',
+    }
+    widgets = {
+      'username': forms.TextInput(attrs={'placeholder': 'Username'})
+    }
