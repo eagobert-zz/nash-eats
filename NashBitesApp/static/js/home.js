94f16e5f460fc86f9a370e2f857c67f51cde6f9e
eagobert <eagobert@hotmail.com>
2018-09-20 12:57:54 -0500

    form field working in homepage view again

--- NashBitesApp/static/js/home.js
+++ NashBitesApp/static/js/home.js
@@ -26,7 +26,7 @@ function initMap() {
   // Click handler function for encoding search input
   function codeAddress() {
 
-    var address = document.getElementById('id_search_input').value;
+    var address = document.getElementById('address').value;
     geocoder.geocode( { 'address': address}, function(results, status) {
 
     if (status == 'OK') {
@@ -42,64 +42,64 @@ function initMap() {
 }
 
 // Fetch location_list view which returns Location data
-fetch('/location/')
+// fetch('/location/')
 
-// Then take the returned data and convert to JSON
-.then(response => {
-    return response.json()
-})
-.then(data => {
+// // Then take the returned data and convert to JSON
+// .then(response => {
+//     return response.json()
+// })
+// .then(data => {
 
-  // Sort data by timestamp in descending order
-  data_sort = data.sort(function(a, b){
+//   // Sort data by timestamp in descending order
+//   data_sort = data.sort(function(a, b){
 
-    var timeA = new Date(b.timestamp).getTime();
-    var timeB = new Date(a.timestamp).getTime();
-    return timeA > timeB ? 1 : -1;
-  })
-  return data_sort
-})
-.then(data_sort => {
+//     var timeA = new Date(b.timestamp).getTime();
+//     var timeB = new Date(a.timestamp).getTime();
+//     return timeA > timeB ? 1 : -1;
+//   })
+//   return data_sort
+// })
+// .then(data_sort => {
   
-  var seen = [];
-  var destinations = [];
+//   var seen = [];
+//   var destinations = [];
 
-  // Loop thru sorted data
-  for (var i = 0; i < data_sort.length; i++){
-
-
-    //If data is not in the array named "seen"
-    if(!seen.includes(data_sort[i].vendor_id)){
-
-      // Add current address to the 'keep' array
-      destinations.push([ 
-        data_sort[i].address,
-        data_sort[i].latitude,
-        data_sort[i].longitude
-      ]);
-
-      // Add vendor id to the seen array
-      seen.push(data_sort[i].vendor_id);
-
-    }
-  }
-  console.log(destinations)
-  return destinations;
-})
-.then(destinations => {
+//   // Loop thru sorted data
+//   for (var i = 0; i < data_sort.length; i++){
 
 
-  // Loop thru destinations and add marker to google map
-  for (var i = 0; i < destinations.length; i++){
-    var destination = destinations[i];
-    var marker = new google.maps.Marker({
-      position: {lat: destination[1], lng: destination[2]},
-      map: map,
-      title: destination[0]
+//     //If data is not in the array named "seen"
+//     if(!seen.includes(data_sort[i].vendor_id)){
 
-    })
-    marker.setMap(map);
-  }
-})
+//       // Add current address to the 'keep' array
+//       destinations.push([ 
+//         data_sort[i].address,
+//         data_sort[i].latitude,
+//         data_sort[i].longitude
+//       ]);
+
+//       // Add vendor id to the seen array
+//       seen.push(data_sort[i].vendor_id);
+
+//     }
+//   }
+//   console.log(destinations)
+//   return destinations;
+// })
+// .then(destinations => {
+
+
+//   // Loop thru destinations and add marker to google map
+//   for (var i = 0; i < destinations.length; i++){
+//     var destination = destinations[i];
+//     var marker = new google.maps.Marker({
+//       position: {lat: destination[1], lng: destination[2]},
+//       map: map,
+//       title: destination[0]
+
+//     })
+//     marker.setMap(map);
+//   }
+// })
 
 
